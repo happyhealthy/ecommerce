b57b3c3939f0925805ef93476ffce650bfdb0a93
hhm000827 <hauhoman@gmail.com>
2024-02-20 00:27:17 +0800

    add more todo in creditcard

--- src/components/payment/creditcard.js
+++ src/components/payment/creditcard.js
@@ -25,32 +25,27 @@ const PaymentForm = (props) => {
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
+    //todo: get the form value from form instead of state
     //todo: early return if the form is empty
     //todo: validate field
 
     onOk();
-    //todo: clear the state
+    //todo: clear the state and reset form
     //todo: notify success (antd component / react hot toast)
   };
 
   const handleCancel = (evt) => {
     evt.preventDefault();
+    //todo: clear the state and reset form
     onCancel();
-    //todo: clear the state
   };
 
   return (
     <>
       <Divider />
       <div className="flex flex-row gap-4">
-        <Cards
-          number={state.number}
-          expiry={state.expiry}
-          cvc={state.cvc}
-          name={state.name}
-          focused={state.focus}
-        />
-        <form className="flex flex-col gap-4">
+        <Cards number={state.number} expiry={state.expiry} cvc={state.cvc} name={state.name} focused={state.focus} />
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
           <input
             type="tel"
             name="number"
@@ -62,40 +57,14 @@ const PaymentForm = (props) => {
             onFocus={handleInputFocus}
           />
           <small>E.g.: 49..., 51..., 36..., 37...</small>
-          <input
-            type="text"
-            name="name"
-            className="form-control"
-            placeholder="Name"
-            required
-            onChange={handleInputChange}
-            onFocus={handleInputFocus}
-          />
-          <input
-            type="tel"
-            name="expiry"
-            className="form-control"
-            placeholder="Valid Thru"
-            pattern="\d\d/\d\d"
-            required
-            onChange={handleInputChange}
-            onFocus={handleInputFocus}
-          />
-          <input
-            type="tel"
-            name="cvc"
-            className="form-control"
-            placeholder="CVC"
-            pattern="\d{3,4}"
-            required
-            onChange={handleInputChange}
-            onFocus={handleInputFocus}
-          />
+          <input type="text" name="name" className="form-control" placeholder="Name" required onChange={handleInputChange} onFocus={handleInputFocus} />
+          <input type="tel" name="expiry" className="form-control" placeholder="Valid Thru" pattern="\d\d\d\d" required onChange={handleInputChange} onFocus={handleInputFocus} />
+          <input type="tel" name="cvc" className="form-control" placeholder="CVC" pattern="\d{3,4}" required onChange={handleInputChange} onFocus={handleInputFocus} />
           <div className="flex flex-row gap-4">
             <Button danger onClick={handleCancel}>
               Cancel
             </Button>
-            <Button onClick={handleSubmit}>Pay</Button>
+            <input type="submit" className="btn btn-primary" value="Pay" />
           </div>
         </form>
       </div>
